e31236969a204b4edd0c521b64324f9b397ab070
Oliver Wegner <void1976@gmail.com>
2019-06-26 13:06:21 +0200

    pushing version of cljow-log dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[cljow-log "0.1.1"]
+  :dependencies [[cljow-log "0.1.2-SNAPSHOT"]
                  [org.clojure/clojure "1.10.0" :scope "provided"]
                  [org.clojure/core.async "0.4.490"]
                  [ubergraph "0.5.2"]]
